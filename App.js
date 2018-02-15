a560fd9f00a48413530e2c20f849331d6181ceda
Greg P <gperlman27@gmail.com>
2018-02-15 00:54:42 -0500

    abstract firebase config

--- App.js
+++ App.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import { Font, AppLoading } from "expo";
 import { Provider } from 'react-redux';
+import firebaseConfig from './firebase'
 import store from './src/store';
 import * as firebase from 'firebase';
 
 // Initialize Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyCQTcJeU4PeuRapg_6drcPS9NE9CbPEBzE",
-  authDomain: "behest-a2d93.firebaseapp.com",
-  databaseURL: "https://behest-a2d93.firebaseio.com",
-  projectId: "behest-a2d93",
-  storageBucket: "behest-a2d93.appspot.com",
-  messagingSenderId: "665105818131"
-};
-
 firebase.initializeApp(firebaseConfig);
 
 import HomeScreenRouter from './src/screens/Home';
@@ -32,34 +24,6 @@ class App extends React.Component {
     this.setState({ loading: false });
   }
 
-  // _handlePress = () => {
-  //   console.log(this.props.navigation, 'nav')
-  //   this.props.navigation.navigate('Home');
-  // }
-
-  // renderContent() {
-  //   switch (this.props.user) {
-  //     case typeof user == 'object':
-  //       return (
-  //         <View>
-  //           <Button title="Go home" onPress={this._handlePress} />
-  //           <Button
-  //             onPress={() => firebase.auth().signOut()}
-  //             title="Log Out"
-  //           />
-  //         </View>
-  //       );
-  //     case false:
-  //       return (
-  //         <Button
-  //           onPress={() => this.loginWithGoogle()}
-  //           title="Log In"
-  //         />
-  //       );
-  //     default:
-  //       return <Text>Loading...</Text>;
-  //   }
-  // }
 
   render() {
     return (
